1a402dfa0b53b4803dd2b719d4c73124f8959489
miraclemichael018 <miraclemichael018@gmail.com>
2023-08-01 07:53:00 +0000

    some changes made

--- src/appwriteConfig.js
+++ src/appwriteConfig.js
@@ -7,9 +7,7 @@ export const COLLECTION_ID_MESSAGES = import.meta.env
 console.log();
 const client = new Client();
 
-client
-	.setEndpoint("https://cloud.appwrite.io/v1")
-	.setProject("649b026c271d06f3ba3d");
+client.setEndpoint("https://cloud.appwrite.io/v1").setProject(`${PROJECT_ID}`);
 
 export const databases = new Databases(client);
 export const account = new Account(client);
